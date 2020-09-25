b41038675213e5daf351d1fe3f77ed86aeed1bb8
Andres P <rapastranac@gmail.com>
2020-09-25 10:34:34 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,50 +3,37 @@
 #include <iostream>
 #include <thread>
 #include <vector>
-#include <map>
-
 
 #define SIZE 10000
 
 Barrier barrier;
-std::map<int, bool> flag;
 std::vector<std::unique_ptr<std::thread>> threads;
 
 void foo(int id) {
-
 	printf("Thread %d entered foo \n", id);
 	barrier.wait_slave();
 	printf("Thread %d passed barrier\n", id);
 }
 
 void bar() {
-
 	for(int i = 0;i<SIZE;i++){
 		barrier.wait_master();
 	}
-
 }
-
-
 
 int main() {
 	threads.resize(SIZE);
 
-	for (int i = 0; i < SIZE; i++)
-	{
+	for (int i = 0; i < SIZE; i++)	{
 		flag.insert(std::make_pair(i, false));
 		threads[i].reset(new std::thread(foo, i));
 	}
 
 	bar();
 
-
-	for (int i = 0; i < SIZE; i++)
-	{
+	for (int i = 0; i < SIZE; i++)	{
 		threads[i]->join();
 	}
-
-
 
 	return 0;
 }
